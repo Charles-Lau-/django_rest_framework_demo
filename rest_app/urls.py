733eaa0ebd6481503404d4407ea87f86ee800629
pengkunliu <pengkunliu@pengkunlius-MacBook-Pro.local>
2015-08-12 00:37:59 +0800

    use viewsets instead of generic views to make code more concise

--- rest_app/urls.py
+++ rest_app/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import url
 from . import views
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import url,include
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [	
-        url(r'^$', views.api_root),
-		url(r'^blogs/$',views.BlogList.as_view(),name='blogs-list'),
-		url(r'^blogs/(?P<pk>[0-9]+)$',views.BlogDetail.as_view(),name='blog-detail'),
-		url(r'^users/$',views.UserList.as_view(),name='users-list'),
-		url(r'^users/(?P<pk>[0-9]+)$',views.UserDetail.as_view(),name='user-detail'),
-]
+router =  DefaultRouter()
+router.register(r'blogs',views.BlogViewSet)
+router.register(r'users',views.UserViewSet)
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns  =[
+	url(r'^',include(router.urls)),
+	url(r'^api-auth/',include('rest_framework.urls',namespace='rest-framework'))
+	]
+
--- rest_app/views.py
+++ rest_app/views.py
@@ -1,14 +1,15 @@
 from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework import mixins
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.reverse import reverse
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
 from rest_app.models import Blog
 from rest_app.serializers import BlogSerializer,UserSerializer
 from django.contrib.auth.models import User
+
 
 
 class isOwnerOrReadOnly(permissions.BasePermission):
@@ -18,33 +19,16 @@ class isOwnerOrReadOnly(permissions.BasePermission):
 
 		return obj.owner == request.user
 	
-
-class BlogList(generics.ListCreateAPIView):
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,isOwnerOrReadOnly)
-	queryset = Blog.objects.all()		   
-	serializer_class = BlogSerializer
-
-	def perform_create(self,serializer):
-		serializer.save(owner = self.request.user)
-
-
-class BlogDetail(generics.RetrieveUpdateDestroyAPIView):
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,isOwnerOrReadOnly,)
+class BlogViewSet(viewsets.ModelViewSet):
 	queryset = Blog.objects.all()
 	serializer_class = BlogSerializer
-
-
-class UserList(generics.ListAPIView):
+	permission_class = (permissions.IsAuthenticatedOrReadOnly, isOwnerOrReadOnly)
+	
+	def perform_create(self,serializer):
+		serializer.save(owner = self.request.user)
+		
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
-class UserDetail(generics.RetrieveAPIView):
-	queryset=  User.objects.all()
-	serializer_class =UserSerializer
-	
-@api_view(('GET',))
-def api_root(request,format=None):
-	return Response({
-			'users':reverse('users-list',request=request,format=format),
-			'blogs':reverse('blogs-list',request=request,format=format)
-				})
+
